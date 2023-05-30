026e7a8df52ec4675bdf7f7b713ebd5e7c194946
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2023-05-30 09:40:52 -0700

    Created HackDay React Template

--- hackday/src/App.js
+++ hackday/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [search, setSearch] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Search for a GitHub organization</h1>
+      <div>
+        <div>
+          <form>
+            <div>
+              <label htmlFor="githubOrgSearch" hidden>
+                Search for GitHub organization
+              </label>
+              <input
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  type="text"
+                  className="form-control"
+                  id="githubOrgSearch"
+                  aria-describedby="githubOrgSearch"
+                  placeholder="Search for a GitHub Organization"
+                />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
